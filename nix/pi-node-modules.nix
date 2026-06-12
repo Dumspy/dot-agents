5787dbd41d3cfbf90a2b0232394e452010b093a2
Felix Berger <felix.enok.berger@gmail.com>
2026-06-12 09:54:55 +0200

    fix: update npm hash for pi-node-modules derivation

--- nix/pi-node-modules.nix
+++ nix/pi-node-modules.nix
@@ -34,7 +34,7 @@ pkgs.stdenvNoCC.mkDerivation {
 
   # NOTE: update this hash after changing dependencies in pi/package.json.
   # Run: nix build --impure --expr 'let pkgs = import <nixpkgs> {}; in pkgs.callPackage ./nix/pi-node-modules.nix {}' --rebuild 2>&1 | grep 'got:'
-  outputHash = "sha256-mztbSQ3vE6Px1XFvh8VGRx/PP2fzkzbKOjKH1rOrxxY=";
+  outputHash = "sha256-ewRP/st5vixYf2ShHbK/gXoJakZvh7UhzRt9+CV7+Cw=";
   outputHashAlgo = "sha256";
   outputHashMode = "recursive";
   dontFixup = true;
